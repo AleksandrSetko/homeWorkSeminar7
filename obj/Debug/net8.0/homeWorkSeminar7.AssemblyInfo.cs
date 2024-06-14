48a0b5b9a04441e29d25ce87d592cbf6970b0f18
Setko AI <setko@msn.com>
2024-06-14 10:34:48 +0300

    Добавлено описание задачи № 2

--- obj/Debug/net8.0/homeWorkSeminar7.AssemblyInfo.cs
+++ obj/Debug/net8.0/homeWorkSeminar7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeWorkSeminar7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b2c8e012a08f6eece4f53f5c47a6cde6b94f0bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab8697866f6fc65373f36a63262504183ef4280")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeWorkSeminar7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeWorkSeminar7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
